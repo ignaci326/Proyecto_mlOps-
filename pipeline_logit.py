8b415655fb367b4f40a345d20714f50cbede95ab
ignaci326 <64497823+ignaci326@users.noreply.github.com>
2024-01-23 19:04:25 -0300

    Update pipeline_logit.py

--- pipeline_logit.py
+++ pipeline_logit.py
@@ -14,7 +14,7 @@ from google_cloud_pipeline_components import aiplatform as gcc_aip
 
 PROJECT_ID = 'manifest-sum-411400'
 DATASET_ID = "diabetes"  # The Data Set ID where the view sits
-TABLE_NAME = "diabetes"  # BigQuery view you create for input data    
+TABLE_NAME = "diabetes"  # BigQuery view you create for input data
     
 @component(
     packages_to_install=["google-cloud-bigquery[pandas]==3.10.0"],
@@ -30,7 +30,7 @@ def export_dataset(
     Args:
         project_id: The Project ID.
         dataset_id: The BigQuery Dataset ID. Must be pre-created in the project.
-        table_name: The BigQuery table name.
+        table_name: The BigQuery view name.
 
     Returns:
         dataset: The Dataset artifact with exported CSV file.
@@ -45,6 +45,7 @@ def export_dataset(
       *
     FROM
       `{table_name}`
+    LIMIT 100
     """.format(
         table_name=table_name
     )
@@ -53,12 +54,129 @@ def export_dataset(
     query_job = client.query(query=query, job_config=job_config)
     df = query_job.result().to_dataframe()
     df.to_csv(dataset.path, index=False)
-  
+    
+@component(
+    packages_to_install=[
+        "pandas==1.3.5",
+        "joblib==1.1.0",
+        "scikit-learn==1.0.2",
+    ],
+)
+def logit_training(
+    dataset: Input[Dataset],
+    model: Output[Model],
+    metrics: Output[Metrics],
+):
+    """Trains an logit classifier.
+
+    Args:
+        dataset: The training dataset.
+
+    Returns:
+        model: The model artifact stores the model.joblib file.
+        metrics: The metrics of the trained model.
+    """
+    import os
+
+    import joblib
+    import pandas as pd
+    from sklearn.metrics import (accuracy_score, precision_recall_curve,
+                                 roc_auc_score)
+    from sklearn.model_selection import (RandomizedSearchCV, StratifiedKFold,
+                                         train_test_split)
+    from sklearn.preprocessing import LabelEncoder
+    from sklearn.linear_model import LogisticRegression
+
+    # Load the training diabetes dataset
+    with open(dataset.path, "r") as train_data:
+        raw_data = pd.read_csv(train_data)
+
+    LABEL_COLUMN = "Outcome"
+
+    X = raw_data.drop([LABEL_COLUMN], axis=1).values
+    y = raw_data[LABEL_COLUMN] 
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+    # Set regularization rate
+    reg = 0.01
+
+    # train a logistic regression model on the training set
+    model = LogisticRegression(C=1/reg, solver="liblinear").fit(X_train, y_train)
+
+    predictions = model.predict(X_test)
+    from sklearn.metrics import accuracy_score
+    Accuracy = accuracy_score(y_test, predictions)
+
+
+    # Export the model to a file
+    os.makedirs(model.path, exist_ok=True)
+    joblib.dump(model, os.path.join(model.path, "model.joblib"))
+    
+
+@component(
+    packages_to_install=["google-cloud-aiplatform==1.25.0"],
+)
+def deploy_logit_model(
+    model: Input[Model],
+    project_id: str,
+    vertex_endpoint: Output[Artifact],
+    vertex_model: Output[Model],
+):
+    """Deploys an XGBoost model to Vertex AI Endpoint.
+
+    Args:
+        model: The model to deploy.
+        project_id: The project ID of the Vertex AI Endpoint.
+
+    Returns:
+        vertex_endpoint: The deployed Vertex AI Endpoint.
+        vertex_model: The deployed Vertex AI Model.
+    """
+    from google.cloud import aiplatform
+
+    aiplatform.init(project=project_id)
+
+    deployed_model = aiplatform.Model.upload(
+        display_name="diabetes-demo-model",
+        artifact_uri=model.uri,
+        serving_container_image_uri="us-docker.pkg.dev/vertex-ai/prediction/xgboost-cpu.1-6:latest",
+    )
+    endpoint = deployed_model.deploy(machine_type="n1-standard-4")
+
+    vertex_endpoint.uri = endpoint.resource_name
+    vertex_model.uri = deployed_model.resource_name
+    
 @dsl.pipeline(
-    name="census-demo-pipeline",
+    name="diabetes-demo-pipeline",
 )
 def pipeline():
     """A demo pipeline."""
+
+    create_input_view_task = create_diabetes_view(
+        project_id=PROJECT_ID,
+        dataset_id=DATASET_ID,
+        view_name=VIEW_NAME,
+    )
+
+    export_dataset_task = (
+        export_dataset(
+            project_id=PROJECT_ID,
+            dataset_id=DATASET_ID,
+            view_name=VIEW_NAME,
+        )
+        .after(create_input_view_task)
+        .set_caching_options(False)
+    )
+
+    training_task = logit_training(
+        dataset=export_dataset_task.outputs["dataset"],
+    )
+
+    _ = deploy_logit_model(
+        project_id=PROJECT_ID,
+        model=training_task.outputs["model"],
+    )
     
 if __name__ == '__main__':
     
@@ -66,4 +184,3 @@ if __name__ == '__main__':
         pipeline_func=pipeline, package_path="tab_classif_pipeline.json"
     )
     print('Pipeline compilado exitosamente')
-    
