febec61c5f17497b61c3e57b190bc3a6d09228e6
ignaci326 <64497823+ignaci326@users.noreply.github.com>
2024-02-01 18:06:52 -0300

    Update execute_pipe_logit.py

--- execute_pipe_logit.py
+++ execute_pipe_logit.py
@@ -6,10 +6,10 @@ if __name__ == '__main__':
     TIMESTAMP = datetime.now().strftime("%Y%m%d%H%M%S")
 
     job = aiplatform.PipelineJob(
-        display_name="mlops-logit",
+        display_name="mlops-xgboost",
         template_path="tab_classif_pipeline.json",
         job_id="mlops-xgboost-{0}".format(TIMESTAMP),
-        enable_caching=True
+        enable_caching=False
     )
 
     job.submit()
