bd11a515f930016105528073f93c7bff20494a3f
ignaci326 <64497823+ignaci326@users.noreply.github.com>
2024-02-21 16:52:43 -0300

    Update pipeline_logit.py

--- pipeline_logit.py
+++ pipeline_logit.py
@@ -12,10 +12,9 @@ from kfp.v2.google.client import AIPlatformClient
 from google.cloud import aiplatform
 from google_cloud_pipeline_components import aiplatform as gcc_aip
 
-PROJECT_ID = 'manifest-sum-411400'
-DATASET_ID = "diabetes"  # The Data Set ID where the view sits
-TABLE_NAME = "diabetes"
-
+PROJECT_ID = 'mlops-414911'
+DATASET_ID = "beans"  # The Data Set ID where the view sits
+TABLE_NAME = "Dry_Bean_Dataset"
     
     
 @component(
@@ -47,7 +46,6 @@ def export_dataset(
       *
     FROM
       `{table_name}`
-    LIMIT 100
     """.format(
         table_name=table_name
     )
@@ -56,23 +54,21 @@ def export_dataset(
     query_job = client.query(query=query, job_config=job_config)
     df = query_job.result().to_dataframe()
     df.to_csv(dataset.path, index=False)
+    
 
-    
-    
 @component(
     packages_to_install=[
-        "xgboost==1.6.2",
         "pandas==1.3.5",
         "joblib==1.1.0",
         "scikit-learn==1.0.2",
     ],
 )
-def xgboost_training(
+def model_training(
     dataset: Input[Dataset],
     model: Output[Model],
     metrics: Output[Metrics],
 ):
-    """Trains an XGBoost classifier.
+    """Trains a model classifier.
 
     Args:
         dataset: The training dataset.
@@ -85,7 +81,7 @@ def xgboost_training(
 
     import joblib
     import pandas as pd
-    import xgboost as xgb
+    from sklearn.tree import DecisionTreeClassifier
     from sklearn.metrics import (accuracy_score, precision_recall_curve,
                                  roc_auc_score)
     from sklearn.model_selection import (RandomizedSearchCV, StratifiedKFold,
@@ -96,73 +92,37 @@ def xgboost_training(
     with open(dataset.path, "r") as train_data:
         raw_data = pd.read_csv(train_data)
 
-    LABEL_COLUMN = "Outcome"
+    LABEL_COLUMN = "Class"
 
     X = raw_data.drop([LABEL_COLUMN], axis=1).values
     y = raw_data[LABEL_COLUMN] 
 
     X_train, X_test, y_train, y_test = train_test_split(X, y)
-    _ = xgb.DMatrix(X_train, label=y_train)
-    _ = xgb.DMatrix(X_test, label=y_test)
 
-    params = {
-        "reg_lambda": [0, 1],
-        "gamma": [1],
-        "max_depth": [10],
-        "learning_rate": [0.1],
-    }
+    clf =  DecisionTreeClassifier()
 
-    xgb_model = xgb.XGBClassifier(
-        n_estimators=50,
-        objective="binary:hinge",
-        silent=True,
-        nthread=1,
-        eval_metric="auc",
-    )
-
-    folds = 3
-    param_comb = 10
-
-    skf = StratifiedKFold(n_splits=folds, shuffle=True, random_state=42)
-
-    random_search = RandomizedSearchCV(
-        xgb_model,
-        param_distributions=params,
-        n_iter=param_comb,
-        scoring="precision",
-        n_jobs=4,
-        cv=skf.split(X_train, y_train),
-        verbose=4,
-        random_state=42,
-    )
-
-    random_search.fit(X_train, y_train)
-    xgb_model_best = random_search.best_estimator_
-    predictions = xgb_model_best.predict(X_test)
+    clf.fit(X_train, y_train)
+    predictions = clf.predict(X_test)
     score = accuracy_score(y_test, predictions)
-    auc = roc_auc_score(y_test, predictions)
-    _ = precision_recall_curve(y_test, predictions)
 
     metrics.log_metric("accuracy", (score * 100.0))
-    metrics.log_metric("framework", "xgboost")
+    metrics.log_metric("framework", "decisionTree")
     metrics.log_metric("dataset_size", len(raw_data))
-    metrics.log_metric("AUC", auc)
 
     # Export the model to a file
     os.makedirs(model.path, exist_ok=True)
-    joblib.dump(xgb_model_best, os.path.join(model.path, "model.joblib"))
+    joblib.dump(clf, os.path.join(model.path, "model.joblib"))
     
-
 @component(
     packages_to_install=["google-cloud-aiplatform==1.25.0"],
 )
-def deploy_xgboost_model(
+def deploy_model(
     model: Input[Model],
     project_id: str,
     vertex_endpoint: Output[Artifact],
     vertex_model: Output[Model],
 ):
-    """Deploys an XGBoost model to Vertex AI Endpoint.
+    """Deploys a model to Vertex AI Endpoint.
 
     Args:
         model: The model to deploy.
@@ -177,9 +137,9 @@ def deploy_xgboost_model(
     aiplatform.init(project=project_id)
 
     deployed_model = aiplatform.Model.upload(
-        display_name="census-demo-model",
+        display_name="beans-demo-model",
         artifact_uri=model.uri,
-        serving_container_image_uri="us-docker.pkg.dev/vertex-ai/prediction/xgboost-cpu.1-6:latest",
+        serving_container_image_uri="us-docker.pkg.dev/vertex-ai/prediction/sklearn-cpu.0-24:latest",
     )
     endpoint = deployed_model.deploy(machine_type="n1-standard-4")
 
@@ -187,28 +147,35 @@ def deploy_xgboost_model(
     vertex_model.uri = deployed_model.resource_name
     
 @dsl.pipeline(
-    name="census-demo-pipeline",
+    name="beans-demo-pipeline",
+    pipeline_root=PIPELINE_ROOT
 )
-def pipeline():
-    """A demo pipeline."""
-
-    export_dataset_task = (
-        export_dataset(
-            project_id=PROJECT_ID,
-            dataset_id=DATASET_ID,
-            table_name=TABLE_NAME,
-        )
-    )
-
-    training_task = xgboost_training(
-        dataset=export_dataset_task.outputs["dataset"],
-    )
-
-    _ = deploy_xgboost_model(
-        project_id=PROJECT_ID,
-        model=training_task.outputs["model"],
-    )
+def pipeline(    
+    bq_source: str = 'bq://mlops-414911.beans.Dry_Bean_Dataset',
+    bucket: str = BUCKET_NAME,
+    project: str = PROJECT_ID,
+    gcp_region: str = REGION,
+    bq_dest: str = "",
+    container_uri: str = "",
+    batch_destination: str = ""):
     
+        export_dataset_task = (
+            export_dataset(
+                project_id=PROJECT_ID,
+                dataset_id=DATASET_ID,
+                table_name=TABLE_NAME,
+            )
+        )
+        
+        training_task = model_training(
+            dataset=export_dataset_task.outputs["dataset"],
+        )
+        
+        _ = deploy_model(
+                project_id=PROJECT_ID,
+                model=training_task.outputs["model"],
+        )
+        
 if __name__ == '__main__':
     
     compiler.Compiler().compile(
